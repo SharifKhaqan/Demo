6f3d368c4517bdb8cbfcf0c903581eb23f9f58c7
SharifKhaqan <sharifkhaqan5@gmail.com>
2025-07-25 17:21:40 +0500

    Changes in the feature-1 branch

--- src/components/Templatelit.jsx
+++ src/components/Templatelit.jsx
@@ -1,9 +1,11 @@
 export default function Templatelit() {
     let name="Sharif Khaqan";
     const age=22;
+    let subject="Computer"
     return (
     <>
     <p>{`My name is ${name} and my age is ${22}.Learing Template Literals`}.</p>
+    <p>{`My favourite subject is ${subject}`}</p>
     </>
   )
 }
